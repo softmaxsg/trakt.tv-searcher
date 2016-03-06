6c04b0aacabe13b501044a68979de5b246b969f5
Vitaly Chupryk <vitaly@softmaxsg.com>
2016-03-06 10:50:56 +0100

    Initial project files upload

---
+++ TraktSearcher/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  TraktSearcher
+//
+//  Copyright © 2016 Vitaly Chupryk. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+    {
+        return true
+    }
+}
+
