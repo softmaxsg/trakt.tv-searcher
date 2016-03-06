059e3c1c5ce84a5a593f7044377df4d2b915c8da
Vitaly Chupryk <vitaly@softmaxsg.com>
2016-03-06 12:54:31 +0100

    Initial upload of TrackSearchAPI framework

---
+++ TracktSearchAPI/TracktSearchAPI.h
@@ -0,0 +1,18 @@
+//
+//  TracktSearchAPI.h
+//  TracktSearchAPI
+//
+//  Copyright © 2016 Vitaly Chupryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TracktSearchAPI.
+FOUNDATION_EXPORT double TracktSearchAPIVersionNumber;
+
+//! Project version string for TracktSearchAPI.
+FOUNDATION_EXPORT const unsigned char TracktSearchAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TracktSearchAPI/PublicHeader.h>
+
+
